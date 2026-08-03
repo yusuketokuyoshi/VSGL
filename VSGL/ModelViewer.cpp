5f6bc60c31e02a2910e1eacf73c1f972871411fc
Yusuke Tokuyoshi <46786472+yusuketokuyoshi@users.noreply.github.com>
2026-08-03 23:27:40 +0900

    Set DPI awareness

--- VSGL/ModelViewer.cpp
+++ VSGL/ModelViewer.cpp
@@ -94,6 +94,7 @@ void ModelViewer::RenderScene()
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPWSTR /*lpCmdLine*/, _In_ int nShowCmd)
 {
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 	vsgl::ModelViewer modelViewer;
 	return GameCore::RunApplication(modelViewer, L"ModelViewer", hInstance, nShowCmd);
 }
